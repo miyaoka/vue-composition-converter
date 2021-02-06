8b0beb2010c1aefbd570374e144ad86762f8f585
Masaya Kazama <miyan@t-p.jp>
2021-02-06 18:12:26 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4 w-screen h-screen">
+    <TheConverter />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TheConverter from './components/TheConverter.vue'
+import './index.css'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TheConverter,
+  },
+})
+</script>
---
+++ src/components/TheConverter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-row h-full">
+    <div class="flex-1 flex flex-col">
+      <h2>Input: (Option API)</h2>
+      <textarea
+        class="border w-full text-xs leading-3 flex-1"
+        v-model="input"
+      ></textarea>
+    </div>
+    <div class="flex-1 flex flex-col">
+      <h2>
+        Output: (<a
+          href="https://v3.vuejs.org/guide/composition-api-introduction.html"
+          >Composition API</a
+        >)
+      </h2>
+      <textarea
+        class="border w-full text-xs leading-3 flex-1"
+        v-model="input"
+        disabled
+      ></textarea>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import text from '../assets/sampleSFC.txt?raw'
+
+export default defineComponent({
+  setup: () => {
+    const input = ref(text)
+    return { input }
+  },
+})
+</script>
