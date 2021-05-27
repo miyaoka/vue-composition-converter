e004afc9c7ed38c04592c4788c84c34a89ec98f4
Masaya Kazama <miyan@t-p.jp>
2021-05-27 18:03:21 +0900

    fix transform of setupContext props

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -116,12 +116,25 @@ export const getMethodExpression = (
   return []
 }
 
+const contextProps = [
+  'attrs',
+  'slots',
+  'parent',
+  'root',
+  'listeners',
+  'refs',
+  'emit',
+]
+
 export const replaceThisContext = (
   str: string,
   refNameMap: Map<string, true>
 ) => {
   return str
-    .replace(/this\.\$/g, 'ctx.root.$')
+    .replace(/this\.\$(\w+)/g, (_, p1) => {
+      if (contextProps.includes(p1)) return `ctx.${p1}`
+      return `ctx.root.$${p1}`
+    })
     .replace(/this\.([\w-]+)/g, (_, p1) => {
       return refNameMap.has(p1) ? `${p1}.value` : p1
     })
