826d08bf4794a732a13807210fb0ff50201bf597
Masaya Kazama <miyan@t-p.jp>
2021-11-05 15:54:20 +0900

    select all text in output

--- src/components/TheConverter.vue
+++ src/components/TheConverter.vue
@@ -18,7 +18,7 @@
     <div class="flex-1 flex flex-col">
       <h2>Output: (Vue2 / Composition API)</h2>
       <pre
-        class="hljs border w-full text-xs leading-3 flex-1 p-2 whitespace-pre-wrap"
+        class="hljs border w-full text-xs leading-3 flex-1 p-2 whitespace-pre-wrap select-all"
         v-html="output"
       />
     </div>
