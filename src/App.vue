86962a6952aef161b08a754a816cbde843c151eb
Masaya Kazama <miyan@t-p.jp>
2022-04-22 11:50:15 +0900

    script setup化

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
+<script lang="ts" setup>
+import TheConverter from "./components/TheConverter.vue";
+import "./index.css";
+</script>
+
 <template>
   <div class="p-4 w-screen h-screen">
     <TheConverter />
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-import TheConverter from './components/TheConverter.vue'
-import './index.css'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    TheConverter,
-  },
-})
-</script>
--- src/components/TheConverter.vue
+++ src/components/TheConverter.vue
@@ -1,3 +1,64 @@
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+import prettier from "prettier";
+import parserTypeScript from "prettier/parser-typescript";
+import hljs from "highlight.js/lib/core";
+import typescript from "highlight.js/lib/languages/typescript";
+import "highlight.js/styles/atom-one-dark.css";
+import { convertSrc } from "../lib/converter";
+import classApi from "../assets/template/classAPI.txt?raw";
+import optionsApi from "../assets/template/optionsAPI.txt?raw";
+
+hljs.registerLanguage("typescript", typescript);
+
+const templateMap = new Map([
+  ["optionsAPI", optionsApi],
+  ["classAPI", classApi],
+]);
+
+const input = ref("");
+const output = ref("");
+const hasError = ref(false);
+const templateKeys = Array.from(templateMap.keys());
+
+const selectedTemplate = ref(templateKeys[0]);
+watch(
+  selectedTemplate,
+  async () => {
+    hasError.value = false;
+    try {
+      input.value = templateMap.get(selectedTemplate.value) || "";
+      console.log(input.value);
+    } catch (err) {
+      hasError.value = true;
+      console.error(err);
+    }
+  },
+  { immediate: true }
+);
+
+watch(
+  input,
+  () => {
+    try {
+      hasError.value = false;
+      const outputText = convertSrc(input.value);
+      const prettifiedHtml = hljs.highlightAuto(
+        prettier.format(outputText, {
+          parser: "typescript",
+          plugins: [parserTypeScript],
+        })
+      ).value;
+      output.value = prettifiedHtml;
+    } catch (err) {
+      hasError.value = true;
+      console.error(err);
+    }
+  },
+  { immediate: true }
+);
+</script>
+
 <template>
   <div class="flex flex-row h-full">
     <div class="flex-1 flex flex-col">
@@ -36,70 +97,6 @@
   </div>
 </template>
 
-<script lang="ts">
-import { ref, defineComponent, watch } from "vue";
-import { convertSrc } from "../lib/converter";
-import classApi from "../assets/template/classAPI.txt?raw";
-import optionsApi from "../assets/template/optionsAPI.txt?raw";
-
-import prettier from "prettier";
-import parserTypeScript from "prettier/parser-typescript";
-import hljs from "highlight.js/lib/core";
-import typescript from "highlight.js/lib/languages/typescript";
-hljs.registerLanguage("typescript", typescript);
-import "highlight.js/styles/atom-one-dark.css";
-
-const templateMap = new Map([
-  ["optionsAPI", optionsApi],
-  ["classAPI", classApi],
-]);
-export default defineComponent({
-  setup: () => {
-    const input = ref("");
-    const output = ref("");
-    const hasError = ref(false);
-    const templateKeys = Array.from(templateMap.keys());
-
-    const selectedTemplate = ref(templateKeys[0]);
-    watch(
-      selectedTemplate,
-      async () => {
-        hasError.value = false;
-        try {
-          input.value = templateMap.get(selectedTemplate.value) || "";
-          console.log(input.value);
-        } catch (err) {
-          hasError.value = true;
-          console.error(err);
-        }
-      },
-      { immediate: true }
-    );
-
-    watch(
-      input,
-      () => {
-        try {
-          hasError.value = false;
-          const outputText = convertSrc(input.value);
-          const prettifiedHtml = hljs.highlightAuto(
-            prettier.format(outputText, {
-              parser: "typescript",
-              plugins: [parserTypeScript],
-            })
-          ).value;
-          output.value = prettifiedHtml;
-        } catch (err) {
-          hasError.value = true;
-          console.error(err);
-        }
-      },
-      { immediate: true }
-    );
-    return { input, output, hasError, templateKeys, selectedTemplate };
-  },
-});
-</script>
 <style scoped>
 .hasError {
   @apply border-4 border-red-500 outline-none;
